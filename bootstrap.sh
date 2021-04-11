4c77b166d14b09e2ec6eb91369d9237fcfabeeed
Pat Sissons <pat.sissons@shopify.com>
2021-04-11 14:40:48 -0700

    auto-install oh-my-zsh

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+# auto-install oh-my-zsh
+[ ! -f "${HOME}/.oh-my-zsh" ] && sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
 # .dotfiles to bootstrap
 declare -a BOOTSTRAP_FILES=(
   .oh-my-zsh
