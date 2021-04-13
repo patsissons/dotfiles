29f24b9f7b52159e94f7e1217cfee103f2761172
Pat Sissons <pat.sissons@shopify.com>
2021-04-13 09:57:30 -0700

    no longer need to install python for spin

--- bootstrap.sh
+++ bootstrap.sh
@@ -20,9 +20,8 @@ done
 if [ ! -z "${SPIN}" ]; then
   # spin packages that need to be installed
   declare -a SPIN_PACKAGES=(
-    # used for git-prompt
-    python
   )
 
-  sudo apt-get install -y --no-install-recommends ${SPIN_PACKAGES}
+  [ ${#SPIN_PACKAGES[@]} -gt 0 ] && \
+    sudo apt-get install -y --no-install-recommends ${SPIN_PACKAGES}
 fi
