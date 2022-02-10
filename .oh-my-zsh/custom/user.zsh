4e6607288f3966b6aa70cb6e1aca537f4ac1d6d8
Pat Sissons <pat.sissons@shopify.com>
2022-02-09 21:27:58 -0800

    fixing homebrew shellenv injection

--- .oh-my-zsh/custom/user.zsh
+++ .oh-my-zsh/custom/user.zsh
@@ -2,6 +2,7 @@
 
 # homebrew integration
 [ -f "/opt/homebrew/bin/brew" ] && eval "$(/opt/homebrew/bin/brew shellenv)"
+[ -f "/usr/local/bin/brew" ] && eval "$(/usr/local/bin/brew shellenv)"
 
 # common shell configuration
 [ -f "${HOME}/.commonrc" ] && source "${HOME}/.commonrc"
